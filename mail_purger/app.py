5e28099538749dfcf86dff4050462a1ccd9687bb
rohilvarma <rohilvarma96@gmail.com>
2024-08-12 21:46:58 +0530

    Added docstrings

--- mail_purger/app.py
+++ mail_purger/app.py
@@ -1,17 +1,36 @@
+"""Mail Purger.
+
+This is the entry point module for the Mail Purger application.
+
+This module is responsible for initializing the Mail Purger application,
+establishing a connection to the mail server, and selecting the folder to
+purge based on the provided command-line argument.
+
+"""
+
 from sys import argv
+
 from purger import Purger
 
+
 def main() -> None:
+    """Entry point of the Mail Purger application.
+
+    Initializes the mail purger, establishes a connection to the mail server,
+    and selects the folder to purge based on the provided command-line argument.
+
+    :return: None
+    :rtype: None
+    """
     mail_purger = Purger()
-    mail_purger._open_connection()
-    
-    if mail_purger._is_logged_in:
+    mail_purger.open_connection()
+    if mail_purger.is_logged_in:
         try:
             folder_name = argv[1]
         except IndexError:
             folder_name = "Inbox"
-        mail_purger._select_folder(folder_name)
-    
+        mail_purger.select_folder(folder_name)
+
 
 if __name__ == "__main__":
     main()
--- mail_purger/purger.py
+++ mail_purger/purger.py
@@ -1,32 +1,81 @@
-import imaplib
+import imaplib  # noqa: D100
+from os import environ
+
+from dotenv import load_dotenv
 from printStatus import printStatus
 
 
 class Purger:
+    """Represents a mail purger.
+
+    This class is responsible for establishing a connection to the mail server,
+    selecting the folder to purge, and logging out of the mail server.
+
+    Attributes
+    ----------
+        __imap_host (str): The IMAP host.
+        __imap_user (str): The IMAP user.
+        __imap_pass (str): The IMAP password.
+        is_logged_in (bool): The login status.
+
+    Methods
+    -------
+        open_connection(): Establish a secure connection to the IMAP server using SSL.
+        select_folder(folder_name): Determine a folder on the IMAP server.
+        __del__(): Log out of the IMAP server and update the login status.
+
+    """
+
     def __init__(self) -> None:
+        """Initialize a Purger object.
+
+        Initialize the IMAP host, user, and password, and set the login status to False.
+
+        :return: None
+        :rtype: None
+        """
+        load_dotenv(override=True)
         self.__imap_host = "imap.gmail.com"
         self.__imap_user = "rohilvarma96@gmail.com"
-        self._is_logged_in = False
+        self.__imap_pass = environ.get("APP_KEY")
+        self.is_logged_in = False
 
     def __update_login_status(self) -> None:
-        self._is_logged_in = not self._is_logged_in
+        self.is_logged_in = not self.is_logged_in
 
-    def _select_folder(self, folder_name: str) -> None:
+    def select_folder(self, folder_name: str) -> None:
+        """Determine a folder on the IMAP server.
+
+        :param folder_name: The name of the folder to select.
+        :type folder_name: str
+        :return: None
+        :rtype: None
+        """
         self.__imap.select(folder_name)
-        printStatus.done("{} folder selected".format(folder_name))
+        printStatus.done(f"'{folder_name}' folder selected")
 
-    def _open_connection(self) -> None:
+    def open_connection(self) -> None:
+        """Establish a secure connection to the IMAP server using SSL.
+
+        :return: None
+        :rtype: None
+        """
         try:
             self.__imap = imaplib.IMAP4_SSL(self.__imap_host)
             printStatus.running("Logging in")
             self.__imap.login(self.__imap_user, self.__imap_pass)
             self.__update_login_status()
-            printStatus.updateDone("Login Successful", True)
+            printStatus.updateDone("Login Successful", progressbar=True)
         except Exception:
             printStatus.updateFailed("Login Failed!")
 
     def __del__(self) -> None:
-        if self._is_logged_in:
+        """Log out of the IMAP server and update the login status once the application encounters some error or application reaches end of state.
+
+        :return: None
+        :rtype: None
+        """
+        if self.is_logged_in:
             self.__imap.logout()
             self.__update_login_status()
             printStatus.done("Logged Out Successfully.")
